8a929d72880b49c1c51beb9a7ef44e4f400f69de
Jay <jerry915077@gmail.com>
2023-01-03 21:59:08 +0800

    build: set less preprocessor

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withLess = require('next-with-less')
+const withLess = require('next-with-less');
 const nextConfig = withLess({
   // reactStrictMode: true,
   lessLoaderOptions: {},
-})
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
