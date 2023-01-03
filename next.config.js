015f89f7f8c216dff74350bc39433358087359d5
Jay <jerry915077@gmail.com>
2023-01-03 14:51:15 +0800

    build: add less setting and modify corresponding subname

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withLess = require('next-with-less')
+const nextConfig = withLess({
+  // reactStrictMode: true,
+  lessLoaderOptions: {},
+})
 
 module.exports = nextConfig
